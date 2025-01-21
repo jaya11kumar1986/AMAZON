984e2b49abe0b0b86da9950c3e4da59b49054410
jaya11kumar1986 <jaya11kumar@gmail.com>
2025-01-21 12:32:30 +0530

    Update Multi.java

--- src/test/java/windows/Multi.java
+++ src/test/java/windows/Multi.java
@@ -41,8 +41,8 @@ driver.findElement(By.id("multi")).click();
         System.out.println("Switched to the window with URL: " + driver.getCurrentUrl());
         
         // Perform further actions or close the driver
-        driver.quit();
-		driver.close();
+       // driver.quit();
+		
 
 	}
 
